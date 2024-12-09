952bd9ebb01f4f9bf44d2ba9d97a31cda47dc8b3
Ted Jenkler <tedjenkler@gmail.com>
2024-12-10 00:23:54 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,22 +1,15 @@
 import net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+const server = net.createServer((connection) => {
+    connection.on("data", (data) => {
+        const request_api_key = data.subarray(0, 2);
+        const request_api_version = data.subarray(2, 4);
+        const correlation_id = data.subarray(4, 12);
 
- const server = net.createServer((connection) => {
-   
-    const messageSize = 4;
-    const correlationId = 7;
+        connection.write(correlation_id);
+    });
+});
 
-    const buffer = Buffer.alloc(messageSize + 4);
-
-    buffer.writeUInt32BE(correlationId, messageSize);
-
-    connection.write(buffer);
-
-    connection.end();
- });
-
- server.listen(9092, "127.0.0.1", () => {
-    console.log("Server is listening on port 9092");
- });
+server.listen(9092, "127.0.0.1", () => {
+  console.log("Server is listening on port 9092");
+});
